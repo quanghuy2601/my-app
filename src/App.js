c87fda3dda022e9c8e55adedafc6741dcd53b9f2
quanghuy2601 <quanghuy7927@gmail.com>
2023-04-30 22:46:54 +0700

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DustyFate - I'm Trying
+          DustyFate
         </p>
       </header>
     </div>
